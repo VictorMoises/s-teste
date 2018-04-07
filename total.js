49a54189c7781c960b94b36af8d1e28613a82237
VictorMoises <37968641+VictorMoises@users.noreply.github.com>
2018-04-07 18:42:44 -0300

    Delete total.js

--- total.js
+++
@@ -1,3 +0,0 @@
-exports.run = (client,message,args)=> {
-message.channel.sendMessage(`Estou no total em: \n**Canais**${client.channels.size} \n**Servidores:**${client.guilds.size} \n**Usuarios:**${client.users.size}`);
-}
