8f02a81055e580c4430b359cf387dd3c567f8096
VictorMoises <37968641+VictorMoises@users.noreply.github.com>
2018-04-07 18:43:46 -0300

    Delete varfalas.js

--- commands/varfalas.js
+++
@@ -1,6 +0,0 @@
-var falas = ["Abys","Itachi"]
-exports.run(client, message, args) => {
-   let user = mentions.user.firts();
-   
-message.channel.sendMessage(Deve ser... " + falas[math.round(math.random()*falas.length-1)]);
-
